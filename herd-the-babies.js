0ddd06ef63b2e75b89df5e7221111ad2ab6fc2d5
nax2uk <azlinayeo@gmail.com>
2020-03-27 09:23:10 +0000

    kata solution complete

--- herd-the-babies.js
+++ herd-the-babies.js
@@ -1,3 +1,17 @@
-const herdTheBabies = () => {};
+/*****************************************************************************************
+ * ASCII values:
+ * -- A = 65
+ * -- a = 97
+ ****************************************************************************************/
+const herdTheBabies = str => {
+  const strCopy = [...str];
+  strCopy.sort((a, b) => {
+    const A = a.toUpperCase();
+    const B = b.toUpperCase();
+    if (A === B) return a < b ? -1 : 1;
+    else return A < B ? -1 : 1;
+  });
+  return strCopy.join("");
+};
 
 module.exports = herdTheBabies;
--- spec/herd-the-babies.spec.js
+++ spec/herd-the-babies.spec.js
@@ -1,4 +1,17 @@
 const { expect } = require("chai");
 const herdTheBabies = require("../herd-the-babies");
 
-describe("heardTheBabies", () => {});
+describe("herdTheBabies", () => {
+  it("returns an empty string when given an empty string", () => {
+    expect(herdTheBabies("")).to.equal("");
+  });
+  it("returns a copy of the original string in the same order when passed", () => {});
+  it("returns an alphabetically ordered string when passed a jumpbled up string", () => {
+    expect(herdTheBabies("cba")).to.equal("abc");
+    expect(herdTheBabies("CBA")).to.equal("ABC");
+  });
+  it("returns an alphabetically ordered string where parents are followed by babies", () => {
+    expect(herdTheBabies("BaA")).to.equal("AaB");
+    //expect(herdTheBabies("BaAcDffFccDdCd")).to.equal("AaB");
+  });
+});
